6a23314c346c14829aec7f518afa19d5459c769c
daniel <dmeza@utexas.edu>
2023-01-31 23:38:27 -0600

    Left turn code working. Have not tested thoroughly yet.

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -47,7 +47,7 @@ using namespace ros_helpers;
 DEFINE_double(cp1_distance, 20.0, "Distance to travel for 1D TOC (cp1)");
 DEFINE_double(cp1_curvature, 0.5, "Curvature for arc path (cp1)");
 
-DEFINE_double(cp2_curvature, 0.5, "Curvature for arc path (cp2)");
+DEFINE_double(cp3_curvature, 0.2, "Curvature for arc path (cp3)");
 
 namespace {
 ros::Publisher drive_pub_;
@@ -71,6 +71,16 @@ Vector2f p(0.0, 0.0);
 
 // Latency calculations
 vector<float> prevCommands{};
+
+// Free path length variables
+float min_dist;
+float r1;
+float r2;
+float r_dist;
+float theta;
+float f;
+Vector2f c(0.0, 0.0);
+
 } // namespace
 
 namespace navigation {
@@ -152,21 +162,55 @@ void Navigation::Run() {
   // If odometry has not been initialized, we can't do anything.
   if (!odom_initialized_) return;
 
-  // Calculate free path length between the car and object. 
-  float min_dist = 10.0;
-  for (int i = 0; i < (int)point_cloud_.size(); i++) {
-    p = point_cloud_[i];
-    // Make all coordinates positive
-    if (p.y() < 0) {
-      p.y() = p.y()*(-1);
-    }
-    // Only check point if in front of the car
-    if (p.y() < 0.2405) {
-      if (p.x() < min_dist){
-        min_dist = p.x();
+  // Calculate free path length between the car and object.
+  min_dist = 10.0;
+  // Moving in a straight line
+  if (abs(FLAGS_cp3_curvature) > 1.0) {
+    for (int i = 0; i < (int)point_cloud_.size(); i++) {
+      p = point_cloud_[i];
+      // Make all coordinates positive
+      if (p.y() < 0) {
+        p.y() = p.y()*(-1);
+      }
+      // Only check point if in front of the car
+      if (p.y() < 0.2405) {
+        if (p.x() < min_dist){
+          min_dist = p.x();
+        }
+      }
+    } // At this point, min_dist is the closest point in front of the car.
+  }
+  // Moving along a left-turning arc
+  else if (FLAGS_cp3_curvature > 0.0) {
+    r1 = FLAGS_cp3_curvature - 0.2405;
+    r2 = sqrt(pow(FLAGS_cp3_curvature + 0.2405, 2) + pow(0.5, 2));
+
+    for (int i = 0; i < (int)point_cloud_.size(); i++) {
+      p = point_cloud_[i];
+      c.x() = 0;
+      c.y() = FLAGS_cp3_curvature;
+
+      // Check for obstacle
+      r_dist = sqrt(pow(p.x() - c.x(), 2) + pow(p.y() - c.y(), 2));
+      theta = atan2(p.x(), FLAGS_cp3_curvature - 0.2405);
+      if (r_dist >= r1 && r_dist <= r2 && theta > 0) {
+        f = FLAGS_cp3_curvature * (theta - atan2(0.5, FLAGS_cp3_curvature - p.y()));
+        
+        // Update minimum free path length
+        if (f < min_dist) {
+          min_dist = f;
+        }
       }
     }
-  } // At this point, min_dist is the closest point in front of the car.
+  }
+  // Moving along a right-turning arc 
+  else if (FLAGS_cp3_curvature < 0.0) {
+
+  }
+  // Special case, radius of curvature  = 0
+  else {
+
+  }
 
   // Calculate distance traveled assuming a 0.2s actuation latency
   float distanceLatency = 0.0;
@@ -181,8 +225,8 @@ void Navigation::Run() {
   distanceTraveled = (odom_loc_ - odom_start_loc_).norm();
 
   // Set the control values to issue drive commands:
-  drive_msg_.curvature = 0.0;
   drive_msg_.velocity = controlVelocity;
+  drive_msg_.curvature = FLAGS_cp3_curvature;
 
   // Keep track of the previous velocity commands for latency compensation
   if(prevCommands.size() == 4) {
